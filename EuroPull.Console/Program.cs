58d354ed0179ab699c11bee8cbd9a44f89278c0a
Chris Jones <chris@jonezy.org>
2018-02-13 14:53:00 -0500

    Figured out switching branches
    - All the basic pieces of functionality are ready to go, just need to
    wrap into a windows service, test in dev environment and deploy to
    production.

--- EuroPull.Console/Program.cs
+++ EuroPull.Console/Program.cs
@@ -9,11 +9,11 @@ namespace EuroPull
 {
     class Program
     {
+        private static string _repoUrl = "https://github.com/q4-euro/Solutions.git";
+        private static string _localPath = "/users/jonezy/Projects/Solutions/";
+
         static void Main(string[] args)
         {
-            var repoUrl = "https://github.com/q4-euro/Solutions.git";
-            var localPath = "/users/jonezy/Projects/Solutions/";
-
             Console.WriteLine("Please enter your github.com usename:");
             var username = Console.ReadLine();
 
@@ -25,17 +25,24 @@ namespace EuroPull
                 CredentialsProvider = (_url, _username, _password) => new UsernamePasswordCredentials { Username = username, Password = password }
             };
 
-            var isCloned = Directory.Exists(Path.GetFullPath(localPath));
+            var isCloned = Directory.Exists(Path.GetFullPath(_localPath));
             if(!isCloned) 
             {
-                Console.WriteLine("Checking out {0} to {1}. This may take a few minutes.", repoUrl, localPath);
+                Console.WriteLine("Checking out {0} to {1}. This may take a few minutes.", _repoUrl, _localPath);
 
-                Repository.Clone(repoUrl, localPath, cloneOptions);
+                Repository.Clone(_repoUrl, _localPath, cloneOptions);
 
                 Console.WriteLine("Checkout complete");    
             } else {
                 string logMessage = "";
-                using (var repo = new Repository(localPath))
+
+                Console.WriteLine("Switch to branch: ");
+
+                var newBranch = Console.ReadLine();
+
+                ChangeBranch(newBranch ?? "master");
+
+                using (var repo = new Repository(_localPath))
                 {
                     FetchOptions options = new FetchOptions();
                     options.CredentialsProvider = new CredentialsHandler((url, usernameFromUrl, types) =>
@@ -53,6 +60,23 @@ namespace EuroPull
                     }
                 }
                 Console.WriteLine(logMessage);
+            }
+        }
+
+        static void ChangeBranch(string newBranch)
+        {
+            // the master branch friendly name is origin/master
+            // all other branches exclude origin/
+            if (newBranch == "master")
+                newBranch = "origin/master";
+            
+            using (var repo = new Repository(_localPath))
+            {
+                var branch = repo.Branches[newBranch];
+                if (branch == null)
+                    return;
+                
+                Commands.Checkout(repo, branch);
             }
         }
     }
